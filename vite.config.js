fe775f6022788c533e0d08537f00cb99267b08d8
Tinnaphop <74139538+LeoDestiny69@users.noreply.github.com>
2025-06-17 16:42:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TomYumMaster/',
+  base: '/tomyummaster/',
   plugins: [react(),tailwindcss()],
 })
